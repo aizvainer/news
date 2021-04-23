7ae9bf75aea9e5b21a84b8808adeeb6b300e5004
aizvainer <vitali.zaikov@gmail.com>
2021-04-23 19:01:54 +0300

    Sprint 3

--- controller/Controller.php
+++ controller/Controller.php
@@ -29,5 +29,26 @@ class Controller {
     public static function error404() {
         include_once 'view/error404.php';
     }
+
+    public static function InsertComment($c, $id) {
+        Comments::InsertComment($c, $id);
+        header('location:news?id='.$id.'#ctable');
+    }
+
+    public static function Comments($newsid) {
+        $arr = Comments::getCommentByNewsID($newsid);
+        ViewComments::CommentsByNews($arr);
+    }
+
+    public static function CommentsCount($newsid) {
+        $arr = Comments::getCommentsCountByNewsID($newsid);
+        ViewComments::CommentsCount($arr);
+    }
+
+    public static function CommentsCountWithAncor($newsid) {
+        $arr = Comments::getCommentsCountByNewsID($newsid);
+        ViewComments::CommentsCountWithAncor($arr);
+    }
+
 }
 ?>
--- view/news.php
+++ view/news.php
@@ -16,7 +16,7 @@ class ViewNews {
     {
         foreach ($arr as $value) {
             echo '<li>'.$value['title'];
-
+            Controller::CommentsCount(($value['id']));
             echo '<a href="news?id='.$value['id'].'">Edasi</a></li><br>';
         }
     }
@@ -24,8 +24,8 @@ class ViewNews {
     public static function ReadNews($n)
     {
         echo '<h2>'.$n['title'].'</h2>';
-
-        //echo '<br><img src="data:image/jpeg:base64,'.base64_encode($n['picture']).'" width=150/><br>';
+        Controller::CommentsCountWithAncor($n['id']);
+        echo '<br><img src="data:image/jpeg:base64,'.base64_encode($n['picture']).'" width=150/><br>';
 
         echo '<p>'.$n['text'].'</p>';
     }
